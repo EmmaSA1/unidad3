542d05b11527469a6f99eef3b798c26f3f8bc993
Leo-Noriega <leonoriega100@gmail.com>
2025-06-30 16:02:36 -0600

    fix: added swagger config

--- src/main/java/utez/edu/mx/unidad3/utils/SwaggerConfig.java
+++ src/main/java/utez/edu/mx/unidad3/utils/SwaggerConfig.java
@@ -1,4 +1,16 @@
 package utez.edu.mx.unidad3.utils;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info()
+                .title("API REST de almacenes")
+                .description("Documentacion de los endpoints del servicio de almacenes")
+                .version("V1.0")
+        );
+    }
 }
